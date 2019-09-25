9453f26a8a1a813acc3871e24aec860fb3e2598a
Vekoks <veselin.k.krustev@gmail.com>
2019-09-25 19:02:23 +0300

    Create handle class.

---
+++ ManlyOrders/ManlyOrders/Models/Handle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManlyOrders.Models
+{
+    public class Handle
+    {
+        public string NmaeKnife { get; set; }
+
+        public string HandleColor { get; set; }
+
+        public int Count { get; set; }
+    }
+}
